93bfdcb3ce4cf453a8be4c4883f7911ea34c0392
Dylan Ward <dylan.ward@iinet.net.au>
2023-07-13 21:18:45 +1000

    setup vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    dts({
+      insertTypesEntry: true,
+    })
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'React-Lib',
+      formats: ['es', 'umd'],
+      fileName: (format) => `react-lib.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
 })
